25a5cf69357991904fde6fd236cc4ad19455c00e
Zach Upstone <zu213@bath.ac.uk>
2025-05-23 13:37:34 +0100

    Added initial seed

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { id: '1' },
+    update: {},
+    create: {
+      title: 'Alice',
+      rating: 3
+    },
+  })
+  console.log({ alice })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
