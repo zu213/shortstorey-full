d7fadd0add16d9b01ebc03c69539ac00d78760c8
Zach Upstone <zu213@bath.ac.uk>
2025-05-26 17:50:38 +0100

    Fixed up initial database seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,16 +1,58 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Story, User } from '@prisma/client'
+
 const prisma = new PrismaClient()
-async function main() {
-  const alice = await prisma.user.upsert({
-    where: { id: '1' },
-    update: {},
-    create: {
-      title: 'Alice',
+
+async function insertUsers() {
+  const alice = await prisma.user.create({
+    data: {
+      name: 'Alice',
       rating: 3
     },
   })
   console.log({ alice })
+  return [alice]
 }
+
+async function insertStories(users: User[]) {
+  const story = await prisma.story.create({
+    data: {
+      title: 'Alice\' story',
+      content: 'story content',
+      user: {
+        connect: { id: users[0].id }, // Correct way to relate to existing user
+      },
+      created_at: new Date()
+    },
+  })
+  console.log({ story })
+  return [story]
+}
+
+async function insertRatings(users: User[], stories: Story[]) {
+  const alice = await prisma.rating.create({
+    data: {
+      created_at: new Date(),
+      actual_score: 4,
+      user_id: users[0].id,
+      user_score: users[0].rating ?? 3,
+      final_score: 4,
+      to_story:  {
+        connect: { id: stories[0].id }, // Correct way to relate to existing user
+      },
+    },
+  })
+console.log({ alice })
+}
+
+async function main() {
+  await prisma.rating.deleteMany();
+  await prisma.story.deleteMany();
+  await prisma.user.deleteMany();
+  const users = await insertUsers()
+  const stories = await insertStories(users)
+  await insertRatings(users, stories)
+}
+
 main()
   .then(async () => {
     await prisma.$disconnect()
